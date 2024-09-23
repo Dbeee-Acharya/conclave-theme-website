63f900fae886c15a900439a0a052f140f7df7f92
dbeee <dbeee.acharya@gmail.com>
2024-09-23 17:37:24 +0545

    Responsive

---
+++ src/js/navBar.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+console.log('loaded')
+console.log(hamburger)
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-purple': 'rgba(172,149,229,1)',
+        'custom-light-blue': 'rgba(163,201,222,1)',
+        'custom-blue': 'rgba(167,194,223,1)',
+      }
+    },
   },
   plugins: [],
 }
